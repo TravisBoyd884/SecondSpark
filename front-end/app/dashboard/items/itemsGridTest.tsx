f2e7c60aa7c0ae1221e55eb6ff66754edde27326
Sean Collins <yubtubfromtheyubtub@gmail.com>
2025-11-13 14:36:09 -0500

    fix css

---
+++ front-end/app/dashboard/items/itemsGridTest.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { Item } from '@/app/lib/definitions';
+import { item } from '@/app/lib/placeholder-data';
+
+export default function ItemsGridTest() {
+    const [items, setItems] = useState<Item[]>([]);
+    const [selectedItem, setSelectedItem] = useState<Item | null>(null);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        setItems(item);
+        refreshItems();
+    }, []);
+
+    const refreshItems = async () => {
+
+    }
+  
+    const handleViewItem = (item: Item) => {
+      setSelectedItem(item);
+      setShowModal(true);
+    }
+  
+    const handleCloseModal = () => {
+      setShowModal(false);
+    }
+  
+    const handleDeleteItem = () => {
+  
+    }
+
+    return (
+        <div>
+            <div className="flex flex-wrap justify-content-center gap-4 mt-4 mb-4">
+                {items.map((item) => (
+                    <div className="card p-4 shadow-sm rounded-md" key={item.item_id}>
+                        <h2 className="text-lg font-bold">{item.title}</h2>
+                        <p>{item.description}</p>
+                        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 cursor-pointer" onClick={() => handleViewItem(item)}>View</button>
+                        <button className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 cursor-pointer" onClick={() => handleDeleteItem()}>Delete</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
--- front-end/app/dashboard/items/page.tsx
+++ front-end/app/dashboard/items/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import ItemsGrid from './itemsGrid';
+import ItemsGridTest from './itemsGridTest';
 
 export default function Page() {
 
   return (
   <div>
-    <h1>Inventory</h1>
-    <ItemsGrid />
+    <h1 className="text-2xl font-bold text-center">Inventory</h1>
+    <ItemsGridTest />
   </div>
 );
 }
