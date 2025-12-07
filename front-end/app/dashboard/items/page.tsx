28b2aef71a86a6ea5900c456710f1ab0a84fc686
TravisBoyd884 <phtboyd884@gmail.com>
2025-12-07 15:12:33 -0500

    Go the items grid test page to work

--- front-end/app/dashboard/items/page.tsx
+++ front-end/app/dashboard/items/page.tsx
@@ -1,11 +1,18 @@
-"use client";
+// app/dashboard/items/page.tsx
+import { getCurrentUser } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
 import ItemsGridTest from "./itemsGridTest";
 
-export default function Page() {
+export default async function Page() {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
   return (
     <div>
       <h1 className="text-2xl font-bold text-center">Inventory</h1>
-      <ItemsGridTest />
+      <ItemsGridTest userId={user.user_id} />;
     </div>
   );
 }
